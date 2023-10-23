700d40b5f7c82526dea8548fc0daaf36283de845
jackdan <2114275890@qq.com>
2023-10-23 16:11:19 +0800

    Initial commit

---
+++ src/test/java/com/djk/jkojcodesandbox/JkojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djk.jkojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
